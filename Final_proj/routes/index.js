d73d4f9129851db2fdeaa91f7e04a07824daffbe
Ahn Jung-euk <woodencatty@gmail.com>
2017-11-25 17:50:13 +0900

    basic setup

--- Final_proj/routes/index.js
+++ Final_proj/routes/index.js
@@ -1,6 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+
+let serverIP = "";
+let serverPort = "";
+
+function Setup_device_Socket(){
+  http.createServer((request, response) => {
+    if (request.method == 'POST') {
+      if (request.url == '/identify/information') {
+        IDD_ID = request.headers.idd_id;
+        response.writeHead(200);
+        response.end("gotit");    //IDD에 확인메세지 전송
+        console.log("Hi! "+ IDD_ID);   //환자 식별
+      }
+      else {
+        console.log("error");
+        response.writeHead(404);
+        response.end();
+      }
+    } /* GET method */
+  }).listen(3010, () => {
+    console.log('Socket is Running (3010) ...');
+  });
+}
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login', { });
@@ -13,5 +38,7 @@ router.get('/dashboard', function(req, res, next) {
 router.get('/devicelist', function(req, res, next) {
   res.render('devicelist', {  });
 });
+
+Setup_device_Socket();    
 
 module.exports = router;
