aa3d8b4333f9fa62a7f76be8c95c687b4c3cd3e7
woodencatty <woodencatty@gmail.com>
2017-12-11 13:33:51 +0900

    주석추가

--- Final_proj/routes/index.js
+++ Final_proj/routes/index.js
@@ -3,21 +3,18 @@ var router = express.Router();
 var http = require('http');
 
 
-let serverIP = "";
-let serverPort = "";
-
 function Setup_device_Socket(){
   http.createServer((request, response) => {
     if (request.method == 'POST') {
       if (request.url == '/device/information') {
-        IDD_ID = request.headers.idd_id;
+        Device_ID = request.headers.device_id;
         response.writeHead(200);
-        response.end("gotit");    //IDD에 확인메세지 전송
-        console.log("Hi! "+ IDD_ID);   //환자 식별
+        response.end("gotit");    //기기 확인메세지 전송
+        console.log("Hi! "+ Device_ID);   //기기 식별
       }
       if (request.url == '/device/leave') {
         response.writeHead(200);
-        response.end("gotit. bye");    //IDD에 확인메세지 전송
+        response.end("gotit. bye");    //기기 확인메세지 전송
       }
       else {
         console.log("error");
@@ -32,20 +29,25 @@ function Setup_device_Socket(){
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) { //로그인
   res.render('login', { });
 });
 
-router.get('/dashboard', function(req, res, next) {
+router.get('/', function(req, res, next) { //회원가입
+  res.render('join', { });
+});
+
+
+router.get('/account', function(req, res, next) {//계정 목록
+  res.render('account', {  });
+});
+
+router.get('/dashboard', function(req, res, next) { //메인화면
   res.render('dashboard', {  });
 });
 
-router.get('/device', function(req, res, next) {
+router.get('/device', function(req, res, next) {//기기 목록
   res.render('device', {  });
-});
-
-router.get('/account', function(req, res, next) {
-  res.render('devicelist', {  });
 });
 
 Setup_device_Socket();    
