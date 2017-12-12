06628ad2a3179053a345eae482e5fc686254687f
woodencatty <woodencatty@gmail.com>
2017-12-12 21:25:33 +0900

    change module

--- Device/search_ap.js
+++ Device/search_ap.js
@@ -1,26 +1,18 @@
 const sendData = require('./rest_api.js')   //포스터기기 연결 모듈 import
+var piWifi = require('pi-wifi');
 
-var wifi = require('node-wifi');
 var searched = false;
 
 const fs = require('fs');
 
-
-var exec = require('child_process').exec;
-wifi.init({
-    iface: null // network interface, choose a random wifi interface if set to null 
-});
-
 module.exports = {
     searchDevice: () => {
-        wifi.getCurrentConnections((err, curcon) => {
-            console.log(curcon[0].signal_level);
-            //todo : check signal
-            if (curcon[0].signal_level > connectRange && searched == false) {
+        piWifi.check('APD', function(err, result) {
+            if (err) {
+              return console.error(err.message);
             }
-            if (curcon[0].signal_level < leaveRange && searched == true) {
-            }
-        });
+            console.log(result);
+          });
     }
 }
 
