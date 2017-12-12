fd9499a1aaa467af2d1e9008604442c2bc1695d6
woodencatty <woodencatty@gmail.com>
2017-12-12 21:17:50 +0900

    please

--- Device/device_core.js
+++ Device/device_core.js
@@ -1,6 +1,5 @@
 const scanAP = require('./search_ap.js')   
 const fs = require('fs');
-var piwifi = require('pi-wifi');
 
 function scanInterval(apName, connectRange, leaveRange, password, scanInterval) {
     this.scanInterval = setInterval(() => {
@@ -9,12 +8,6 @@ function scanInterval(apName, connectRange, leaveRange, password, scanInterval) 
   }
 
 function initialize() {
-
-  
-  piwifi.connect('APD', '1q2w3e4r', (err)=>{
-      //TODO : 연결
-      console.log(err);
-  });
 
   fs.readFile('./settings.conf', 'utf8',(err, data)=> {  
     var config = JSON.parse(data);    
