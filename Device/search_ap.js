8a5d4dae838073bc080c85521d50ca3015affdac
woodencatty <woodencatty@gmail.com>
2017-12-12 21:00:02 +0900

    why

--- Device/search_ap.js
+++ Device/search_ap.js
@@ -1,9 +1,6 @@
 const sendData = require('./rest_api.js')   //포스터기기 연결 모듈 import
 
 var wifi = require('node-wifi');
-var searched = false;
-
-const fs = require('fs');
 
 wifi.init({
     iface: null // network interface, choose a random wifi interface if set to null 
@@ -12,16 +9,16 @@ wifi.init({
 module.exports = {
     searchAPD: (apName, password, connectRange, leaveRange) => {
         wifi.getCurrentConnections((err, curcon) => {
-            console.log(curcon[0].signal_level);
+            console.log(curcon);
             //todo : check signal
-            if (curcon[0].signal_level > connectRange && searched == false) {
+            /*if (curcon[0].signal_level > connectRange && searched == false) {
                 sendData.SubmitDeviceName('LED001', "1.0", "LED", "LED전등");
                 searched = true;
             }
             if (curcon[0].signal_level < leaveRange && searched == true) {
                 sendData.SubmitDeviceOut();
                 searched = false;
-            }
+            }*/
         });
     }
 }
