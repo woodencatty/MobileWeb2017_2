13ec7d29bf0e5ae92e0be178425a1a9cca38d367
Ahn Jung-euk <woodencatty@gmail.com>
2017-12-16 18:18:25 +0900

    reset

--- Device/rest_api.js
+++ Device/rest_api.js
@@ -1,6 +1,6 @@
 const http = require('http');										//http 요청 모듈
 
-let serverIP = "192.168.0.12";
+let serverIP = "::ffff:192.168.137.1";
 let serverPort = "3010";
 
 POST_DeviceName = {												//POST요청 JSON데이터 정의
